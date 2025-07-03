0b5a326cf5116febd4f221ebc972e43d2dfeaa17
JEY HIRANO <mrjr0722@me.com>
2025-07-04 00:45:19 +0900

    アクセスセクションに大学の住所を追加

--- src/components/layout/AccessSectionTwo.tsx
+++ src/components/layout/AccessSectionTwo.tsx
@@ -136,59 +136,102 @@ const AccessTime = styled.div`
   }
 `;
 
-const AccessSectionTwo: React.FC = () => {
-    return (
-        <AccessSection>
-            <AccessWrapper>
-                <SectionTitleTwo
-                    englishTitle="Access"
-                    japaneseTitle="アクセス"
-                    isLightTheme={false}
-                />
-                <AccessContent>
-                    <MapContainer>
-                        <MapFrame
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.8280885177866!2d139.7673068!3d35.6809591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sjp!4v1704000000000!5m2!1sja!2sjp"
-                            allowFullScreen
-                            loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade"
-                            title="東京駅周辺地図"
-                        />
-                    </MapContainer>
+// キャンパス情報のスタイルを追加
+const CampusInfo = styled.div`
+  text-align: center;
+  margin-bottom: 32px;
+  padding: 20px;
+  background-color: ${(props) => props.theme.pallet.secondary.main};
+  border-radius: 16px;
 
-                    <AccessInfoContainer>
-                        <AccessCategory>
-                            <CategoryTitle>電車でのアクセス</CategoryTitle>
-                            <AccessItem>
-                                <TransportIcon>XR線</TransportIcon>
-                                <AccessDetails>
-                                    <StationName>「XX駅」南口より徒歩10分</StationName>
-                                    <AccessTime>山手線・京浜東北線が利用可能です。</AccessTime>
-                                </AccessDetails>
-                            </AccessItem>
-                            <AccessItem>
-                                <TransportIcon>AR線</TransportIcon>
-                                <AccessDetails>
-                                    <StationName>「XX駅」西口より徒歩8分</StationName>
-                                    <AccessTime>山手線・京浜東北線が利用可能です。</AccessTime>
-                                </AccessDetails>
-                            </AccessItem>
-                        </AccessCategory>
-                        <AccessCategory>
-                            <CategoryTitle>バスでのアクセス</CategoryTitle>
-                            <AccessItem>
-                                <TransportIcon>市バス</TransportIcon>
-                                <AccessDetails>
-                                    <StationName>「XX駅」西口より徒歩9分</StationName>
-                                    <AccessTime>JR「XX駅」東口バスターミナル3番乗り場よりお乗りください。</AccessTime>
-                                </AccessDetails>
-                            </AccessItem>
-                        </AccessCategory>
-                    </AccessInfoContainer>
-                </AccessContent>
-            </AccessWrapper>
-        </AccessSection>
-    );
+  @media (max-width: 768px) {
+    margin-bottom: 24px;
+    padding: 16px;
+  }
+`;
+
+const CampusName = styled.h3`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: ${(props) => props.theme.pallet.primary.main};
+  margin-bottom: 8px;
+
+  @media (max-width: 768px) {
+    font-size: 1.3rem;
+  }
+`;
+
+const CampusAddress = styled.p`
+  font-size: 1rem;
+  color: ${(props) => props.theme.pallet.heading.main};
+  margin: 0;
+  line-height: 1.4;
+
+  @media (max-width: 768px) {
+    font-size: 0.9rem;
+  }
+`;
+
+const AccessSectionTwo: React.FC = () => {
+  return (
+    <AccessSection>
+      <AccessWrapper>
+        <SectionTitleTwo
+          englishTitle="Access"
+          japaneseTitle="アクセス"
+          isLightTheme={false}
+        />
+        <AccessContent>
+          <MapContainer>
+            <MapFrame
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.8280885177866!2d139.7673068!3d35.6809591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sjp!4v1704000000000!5m2!1sja!2sjp"
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="東京駅周辺地図"
+            />
+          </MapContainer>
+
+          <CampusInfo>
+            <CampusName>東京キャンパス</CampusName>
+            <CampusAddress>
+              〒100-0005 東京都千代田区丸の内 123-456-789<br />
+            </CampusAddress>
+          </CampusInfo>
+
+          <AccessInfoContainer>
+            <AccessCategory>
+              <CategoryTitle>電車でのアクセス</CategoryTitle>
+              <AccessItem>
+                <TransportIcon>JR線</TransportIcon>
+                <AccessDetails>
+                  <StationName>「東京駅」丸の内中央口より徒歩3分</StationName>
+                  <AccessTime>山手線・京浜東北線・東海道線・中央線が利用可能です。</AccessTime>
+                </AccessDetails>
+              </AccessItem>
+              <AccessItem>
+                <TransportIcon>地下鉄</TransportIcon>
+                <AccessDetails>
+                  <StationName>「大手町駅」B7出口より徒歩5分</StationName>
+                  <AccessTime>丸ノ内線・千代田線・半蔵門線・東西線が利用可能です。</AccessTime>
+                </AccessDetails>
+              </AccessItem>
+            </AccessCategory>
+            <AccessCategory>
+              <CategoryTitle>バスでのアクセス</CategoryTitle>
+              <AccessItem>
+                <TransportIcon>都バス</TransportIcon>
+                <AccessDetails>
+                  <StationName>「丸の内南口」バス停より徒歩1分</StationName>
+                  <AccessTime>東京駅丸の内南口バス乗り場よりお乗りください。</AccessTime>
+                </AccessDetails>
+              </AccessItem>
+            </AccessCategory>
+          </AccessInfoContainer>
+        </AccessContent>
+      </AccessWrapper>
+    </AccessSection>
+  );
 };
 
 export default AccessSectionTwo;
